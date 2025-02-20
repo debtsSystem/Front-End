12191eb6d8e8314a8d26fdaa3b59c7d999edc552
sarah-frankel <frankels024@gmail.com>
2025-02-20 15:20:04 +0200

    add models

---
+++ src/app/models/customer.model.ts
@@ -0,0 +1,4 @@
+export interface Customer {
+    costomerId: number;
+    customerName: string;
+}
---
+++ src/app/models/debts.model.ts
@@ -0,0 +1,11 @@
+
+export interface Debts {
+    debtId: number;
+    customerId: number;
+    sumOfDebt: number;
+    saleId: number;
+    isPaid: boolean;
+    date: Date;
+    nots: string;
+    paymentType: number
+}
